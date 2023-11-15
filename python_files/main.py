f14da370ef6d61890c37460f34c26b1d17ceb808
vighnesh_wednesday <vighnesh@wednesday.is>
2023-11-15 17:41:17 +0530

    added verbosity

--- python_files/main.py
+++ python_files/main.py
@@ -78,6 +78,8 @@ insurance_employee = insurance.join(employee, on='AGENT_ID', how='left')
 
 df = insurance_employee.join(vendor, on='VENDOR_ID', how='left')
 
+print("Task 1 Done")
+
 # COMMAND ----------
 
 # task 2: create new column 'colocation'
@@ -87,10 +89,14 @@ cond = ((col('CUSTOMER_STATE') == col('INCIDENT_STATE')) & (col('AGENT_STATE') =
 
 df = df.withColumn('COLOCATION', when(cond, 1).otherwise(0))
 
+print("Task 2 Done")
+
 # task 3: 
 cond = ((col('AUTHORITY_CONTACTED') != 'Police') & (col('POLICE_REPORT_AVAILABLE') == 1))
 
 df = df.withColumn('AUTHORITY_CONTACTED', when(cond, 'Police').otherwise(col('AUTHORITY_CONTACTED')))
+
+print("Task 3 Done")
 
 # COMMAND ----------
 
@@ -127,6 +133,8 @@ claim_deviation = sub.select('TRANSACTION_ID', 'CLAIM_DEVIATION')
 
 df = df.join(claim_deviation, on = 'TRANSACTION_ID', how = 'left')
 
+print("Task 4 Done")
+
 # COMMAND ----------
 
 # task 5: apply discounts & increments in claim_amount
@@ -142,12 +150,16 @@ df = df.withColumn('NEW_PREMIUM', \
         .when(get_cond('Health', 'Property'), sp_round(col('PREMIUM_AMOUNT') * 1.07, 2))\
         .when(get_cond('Life', 'Motor'), sp_round(col('PREMIUM_AMOUNT') * 1.02, 2)).otherwise('PREMIUM_AMOUNT'))
 
+print("Task 5 Done")
+
 # COMMAND ----------
 
 # task 6: create new column 'eligible_for_discount' 
 cond = (col('TENURE') > 60) & (col('EMPLOYMENT_STATUS') == 'N') & (col('NO_OF_FAMILY_MEMBERS') >= 4)
 
 df = df.withColumn('ELIGIBLE_FOR_DISCOUNT', when(cond, 1).otherwise(0))
+
+print("Task 6 Done")
 
 
 # task 7: create new column 'claim_velocity'
@@ -170,6 +182,8 @@ claim_velocity = sub.select('TRANSACTION_ID', 'CLAIM_VELOCITY')
 
 df = df.join(claim_velocity, on = 'TRANSACTION_ID', how = 'left')
 
+print("Task 7 Done")
+
 # COMMAND ----------
 
 # task 8: find all suspicious employees
@@ -177,11 +191,15 @@ cond = ((col('CLAIM_STATUS') == 'A') & (col('RISK_SEGMENTATION') == 'H') & (col(
 
 df = df.withColumn('SUSPICIOUS', when(cond, 1).otherwise(0))
 
+print("Task 8 Done")
+
 # COMMAND ----------
 
 # finally writting the data in transformed container
 df.write.csv('/dbfs/mnt/transformed/final_data.csv', header=True, mode="overwrite")
 
 # COMMAND ----------
+
+print("Execution Complete")
 
 
