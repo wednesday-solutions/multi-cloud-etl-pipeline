8d6730d306cb8515f55d14abb6381daf54e4c1ab
vighnesh_wednesday <vighnesh@wednesday.is>
2023-11-27 12:30:41 +0530

    changes for databricks

--- python_files/Extraction.py
+++ python_files/Extraction.py
@@ -2,6 +2,15 @@ import subprocess
 
 
 def extract_from_kaggle(flag: bool):
+    if flag:
+        zip_path = "/mnt/zipdata/"
+        read_path = "/mnt/rawdata/"
+        write_path = "/mnt/transformed/"
+    else:
+        zip_path = "temp/"
+        read_path = "temp2"
+        write_path = "s3://glue-bucket-vighnesh/transformed/"
+
     command = "pip install kaggle"
     result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     print("Output:", result.stdout)
@@ -9,26 +18,18 @@ def extract_from_kaggle(flag: bool):
     # COMMAND ----------
 
     # downloading dataset zip file in zipdata container
-    command = "mkdir -p temp/ && kaggle datasets download -d mastmustu/insurance-claims-fraud-data -p temp/"
-    try:
-        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        print("Output:", result.stdout)
-    except subprocess.CalledProcessError as e:
-        print("Error:", e)
-        print("Error Output:", e.stderr)
+    command = f"kaggle datasets download -d mastmustu/insurance-claims-fraud-data -p {zip_path}"
+    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    print("Output:", result.stdout)
 
     # COMMAND ----------
 
     # unzip data in rawdata container
-    command = f"unzip -o insurance-claims-fraud-data.zip -d temp/"
+    command = f"unzip -o {zip_path}/insurance-claims-fraud-data.zip -d {read_path}"
     result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     print("Output:", result.stdout)
 
-    if flag:
-        read_path = "/mnt/rawdata/"
-        write_path = "/mnt/transformed/"
-    else:
+    if flag==False:
         read_path = "s3://glue-bucket-vighnesh/rawdata/"
-        write_path = "s3://glue-bucket-vighnesh/transformed/"
 
     return read_path, write_path
