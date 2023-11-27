9cf940e76e14c159885eb533169d39381f0ed754
vighnesh_wednesday <vighnesh@wednesday.is>
2023-11-27 15:38:46 +0530

    added file to connect with aws glue

---
+++ python_files/connect_glue.py
@@ -0,0 +1,15 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+
+def init_glue():    
+    sc = SparkContext()
+    glueContext = GlueContext(sc)
+    spark = glueContext.spark_session
+    job = Job(glueContext)
+    
+    return glueContext, spark, job
