3c06159567434a28e45f6c97b06835ec49299a2d
vighnesh_wednesday <vighnesh@wednesday.is>
2023-11-29 17:39:45 +0530

    fix: changed from bash to direct api

--- app/Extraction.py
+++ app/Extraction.py
@@ -1,32 +1,22 @@
 import subprocess
 import os
+os.system('pip install kaggle')
+import kaggle
 
 
 def extract_from_kaggle(flag: bool):
     if flag:
-        zip_path = "/dbfs/mnt/zipdata/"
         read_path = "/dbfs/mnt/rawdata/"
         write_path = "/mnt/transformed/"
     else:
-        zip_path = "temp-zip/"
         read_path = "temp/"
         write_path = "s3://glue-bucket-vighnesh/transformed/"
 
-    os.system('pip install kaggle')
-
     # COMMAND ----------
 
-    # downloading dataset zip file in zipdata container
-    command = f'kaggle datasets download -d mastmustu/insurance-claims-fraud-data -p {zip_path}'
-    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    print("Output:", result.stdout)
-
-    # COMMAND ----------
-
-    # unzip data in rawdata container
-    command = f"unzip -o {zip_path}insurance-claims-fraud-data.zip -d {read_path}"
-    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    print("Output:", result.stdout)
+    api = kaggle.KaggleApi()
+    api.authenticate()
+    api.dataset_download_cli("mastmustu/insurance-claims-fraud-data", unzip=True, path=read_path)
 
     if flag:
         zip_path = zip_path[5:]
